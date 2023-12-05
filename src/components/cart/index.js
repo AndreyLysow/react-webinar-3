a591611c0967f34d88c3bdb9627faa1c531a6478
Andrey Lysov <77214813+AndreyLysow@users.noreply.github.com>
2023-12-05 15:46:32 +0700

    Update index.js

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import './style.css';
@@ -21,7 +21,20 @@ function CartSummary({ cart, onOpenCart, onRemoveItem }) {
     : [];
 
   const totalItems = cartItems.reduce((acc, item) => acc + item.count, 0);
+  const totalGroups = cartItems.length;
   const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.count, 0);
+
+  useEffect(() => {
+    const handleBodyScroll = () => {
+      document.body.style.overflow = isModalOpen ? 'hidden' : 'auto';
+    };
+
+    document.addEventListener('scroll', handleBodyScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleBodyScroll);
+    };
+  }, [isModalOpen]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -32,7 +45,6 @@ function CartSummary({ cart, onOpenCart, onRemoveItem }) {
   };
 
   const handleRemoveItem = (code) => {
-    // Всегда удаляем полностью
     onRemoveItem(code, true);
   };
 
@@ -41,14 +53,16 @@ function CartSummary({ cart, onOpenCart, onRemoveItem }) {
       backgroundColor: 'rgba(0, 0, 0, 0.5)',
     },
     content: {
-      width: '80vw',
-      maxWidth: 'none',
-      margin: 0, /* Изменено значение марджина на 0 */
+      position: 'absolute',
+      width: '960px',
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
+      zIndex: '3',
       border: 'none',
-      borderRadius: '8px',
+      borderRadius: '10px',
+      overflow: 'auto',
+      padding: '0',
     },
   };
 
@@ -56,41 +70,46 @@ function CartSummary({ cart, onOpenCart, onRemoveItem }) {
     <div className="CartSummary">
       <p>
         {totalItems > 0 ? (
-          <>В корзине: {totalItems} товара / {totalPrice} ₽</>
+          <>
+            В корзине: {totalGroups} {totalGroups === 1 ? 'товар' : totalGroups < 5 ? 'товара' : 'товаров'} / {totalPrice.toLocaleString()} ₽
+          </>
         ) : (
-          <>В корзине: <b>пусто</b></>
+          <>
+            В корзине: <b>пусто</b>
+          </>
         )}
       </p>
       <div className="button-container" onClick={openModal}>
-      <button className="remove-button go-button">Перейти</button>
+        <button className="remove-button go-button">Перейти</button>
       </div>
 
       <Modal isOpen={isModalOpen} onRequestClose={closeModal} style={modalStyle}>
-  <div className="modal-header">
-    <h2>Корзина</h2>
-    <button className="remove-button" onClick={closeModal}>
-      Закрыть
-    </button>
-  </div>
-  {cartItems.map((item, index) => (
-    <div key={item.code} className="item">
-      <div className="item-content">
-        <span className="item-info">{index + 1}. {item.title}</span>
-        <span className="item-details">
-          {item.count} шт. - {item.price * item.count} ₽
-        </span>
-      </div>
-      <button className="remove-button" onClick={() => handleRemoveItem(item.code)}>
-        Удалить
-      </button>
-    </div>
-  ))}
-  {totalItems > 0 && (
-    <div className="total">
-      Итого: {totalPrice} ₽
-    </div>
-  )}
-</Modal>
+        <div className="modal-header">
+          <h2>Корзина</h2>
+          <button className="remove-button" onClick={closeModal}>
+            Закрыть
+          </button>
+        </div>
+        {cartItems.map((item, index) => (
+          <div key={item.code} className="item">
+            <div className="item-content">
+              <span className="item-info">{index + 1} {item.title}</span>
+              <span className="item-details">
+              <span className="item-price">{item.price * item.count} ₽</span>
+              <span className="item-count">{item.count} шт</span>
+              </span>
+            </div>
+            <button className="remove-button" onClick={() => handleRemoveItem(item.code)}>
+              Удалить
+            </button>
+          </div>
+        ))}
+        {totalItems > 0 && (
+          <div className="total">
+            Итого  {totalPrice.toLocaleString()} ₽
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
